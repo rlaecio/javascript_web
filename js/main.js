42bd5f90f33900a1787aa579ca50809ad586c5c4
rlaecio <rlaecio@hotmail.com>
2023-03-12 01:14:47 +0000

    white por for

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
 function tocaSom(idElementoSom){
     document.querySelector(idElementoSom).play();
-
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 let contador = 0;
 
-while(contador < listaDeTeclas.length){
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+   
     const tecla = listaDeTeclas[contador]
     const instrumento = tecla.classList[1];
     const idAudio = `#som_${instrumento}`;
+
     tecla.onclick = function(){
         tocaSom(idAudio);
-    }
-    contador = contador +1;
-
+    } 
 }
+

67e76f8eccd924717b02fa8438470b663fe061dd
rlaecio <rlaecio@hotmail.com>
2023-03-12 02:25:12 +0000

    refactory

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
-function tocaSom(idElementoSom){
-    document.querySelector(idElementoSom).play();
+function tocaSom(seletorAudio){
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento != null && elemento.localName === 'audio') {        
+        elemento.play();        
+    }
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
@@ -14,5 +18,13 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
     tecla.onclick = function(){
         tocaSom(idAudio);
     } 
+    tecla.onkeydown = function (evento) {       
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');            
+        }
+    }
+    tecla.onkeyup = function (){
+        tecla.classList.remove('ativa');
+    }
 }
 
